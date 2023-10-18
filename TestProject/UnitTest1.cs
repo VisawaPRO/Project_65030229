7df94a1e6825873f87977c9cf3aadf8e1f58c960
VisawaPro <visava05@gmail.com>
2023-10-18 10:37:29 +0700

    test1

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -9,5 +9,13 @@ namespace TestProject
         {
             Assert.IsTrue(Enum.GetNames(typeof(Project.Members)).Length == 4);
         }
+        [TestMethod]
+        public void TestProjectMemberIds()
+        {
+            Assert.IsTrue((int)Project.Members.Phet == 1);
+            Assert.IsTrue((int)Project.Members.Kla == 2);
+            Assert.IsTrue((int)Project.Members.Mag == 3);
+            Assert.IsTrue((int)Project.Members.Beck == 4);
+        }
     }
 }
